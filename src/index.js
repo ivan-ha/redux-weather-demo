4ea88633b172e8f81bc465fa4aecbcda3e9fece2
Ivan HA <hangolam@gmail.com>
2017-05-23 00:43:23 +0800

    [UI] implement search bar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    return (
-        <div>Hello World</div>
-    );
-};
+import App from './components/app'
 
 ReactDOM.render(<App />, document.querySelector('.container'));
